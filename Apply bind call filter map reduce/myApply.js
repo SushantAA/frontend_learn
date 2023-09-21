1d7847e08caa02760b4d492d0008a6689a3b5b00
SushantAA <58526162+SushantAA@users.noreply.github.com>
2023-09-21 19:10:02 +0530

    Refactor:call apply

--- Apply bind call filter map reduce/myApply.js
+++ Apply bind call filter map reduce/myApply.js
@@ -1,21 +1,17 @@
 const obj = {
-    a: 'a',
-    b: 'b',
-    fun: function(){
-        console.log(this.a + this.b);
-    }
-}
-
-const obj2 = {
     a:'c',
     b:'d'
 }
 
-obj.fun.apply = function (otherObject, args) {
-    otherObject.fun = obj.fun;
+Function.prototype.susApply = function (otherObject, ...args) {
+    otherObject.fun = this;
     console.log(args);
     otherObject.fun();
     delete otherObject.fun;
 }
 
-obj.fun.apply(obj2, ['1', '2']);
+function fun (){
+   console.log(this.a + this.b);
+}
+
+fun.susApply(obj, ['1', '2']);
--- Apply bind call filter map reduce/myCall.js
+++ Apply bind call filter map reduce/myCall.js
@@ -1,21 +1,17 @@
 const obj = {
-    a: 'a',
-    b: 'b',
-    fun: function(){
-        console.log(this.a + this.b);
-    }
-}
-
-const obj2 = {
     a:'c',
     b:'d'
 }
 
-obj.fun.call = function (otherObject, ...args) {
-    otherObject.fun = obj.fun;
+Function.prototype.susCall = function (otherObject, ...args) {
+    otherObject.fun = this;
     console.log(args);
     otherObject.fun();
     delete otherObject.fun;
 }
 
-obj.fun.call(obj2, '1', '2');
+function fun (){
+   console.log(this.a + this.b);
+}
+
+fun.susCall(obj, '1', '2');
